d809db52dbe3075383f808d8885ce531c9ffbd53
starsilver079 <starsilver079@gmail.com>
2022-11-20 21:48:34 +0900

    FlutterProject implemented by AWS amplify.

---
+++ amplify_trips_planner/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifytripsplanner8272e639": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "api": {
+        "amplifytripsplanner": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "storage": {
+        "s3cee8d26e": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
